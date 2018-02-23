6f1e5f452e36647d42109d581b2396a89b45a497
siasdulren <1113639969@qq.com>
2018-02-23 15:57:31 +0800

    第四次更新

--- server-official-account/app.js
+++ server-official-account/app.js
@@ -1,3 +1,18 @@
 const EXPRESS = require('express');
-let app=new EXPRESS();
+const WECHAT = require('wechat');
+let app = new EXPRESS();
+
+let config = {
+    appid: 'wxe1c53da14bbdb7b9',
+    token: 'weixin',
+    encodingAESKey: 'iRWyy5zxQNVMJHxkBgjHHmR3esLcLBTGWCwo5VItcEe'
+};
+
+app.use(EXPRESS.query());
+
+app.get('/',WECHAT(config,(req,res,next)=>{
+    let message = req.weixin;
+    console.log(message);
+}));
+
 app.listen(3000);
