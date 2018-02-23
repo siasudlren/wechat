c873efe2be27322ba140ab09c28aced8b9b87f00
siasdulren <1113639969@qq.com>
2018-02-23 17:04:45 +0800

    第五次更新

--- server-official-account/app.js
+++ server-official-account/app.js
@@ -5,14 +5,16 @@ let app = new EXPRESS();
 let config = {
     appid: 'wxe1c53da14bbdb7b9',
     token: 'weixin',
-    encodingAESKey: 'iRWyy5zxQNVMJHxkBgjHHmR3esLcLBTGWCwo5VItcEe'
+    encodingAESKey: 'NCcWLM24j36yyhiq88p4J3ACP5NcQrsdZMg4HhUgqhd',
+    checkSignature:true
 };
 
 app.use(EXPRESS.query());
 
-app.get('/',WECHAT(config,(req,res,next)=>{
+app.get('/',WECHAT(config,(req,res)=>{
     let message = req.weixin;
     console.log(message);
+    res.reply('收到');
 }));
 
 app.listen(3000);
