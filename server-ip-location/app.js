50514f41dd3d1945dca18e259d4c538f8555b29b
siasdulren <1113639969@qq.com>
2018-02-27 15:45:02 +0800

    第十二次更新

--- server-ip-location/app.js
+++ server-ip-location/app.js
@@ -6,16 +6,16 @@ let app = new EXPRESS();
 let pool = MYSQL.createPool({
     user: 'root'
 });
-app.get('/',(req,res)=>{
-    let ip =req.query.ip;
-    const SQL='SELECT loc FROM db.ip WHERE INET_ATON(?) BETWEEN INET_ATON(min) AND INET_ATON(max)';
-    pool.query(SQL,[ip],(err,results)=>{
-        if(err) throw err ;
-        if(results.length==1){
+app.get('/', (req, res) => {
+    let ip = req.query.ip;
+    const SQL = 'SELECT loc FROM db.ip WHERE INET_ATON(?) BETWEEN INET_ATON(min) AND INET_ATON(max)';
+    pool.query(SQL, [ip], (err, results) => {
+        if (err) throw err;
+        if (results.length === 1) {
             let loc = results[0].loc;
-            res.send({"status":"OK","loc":loc});
-        }else{
-            res.send({"status":"err"});
+            res.send({"status": "OK", "loc": loc});
+        } else {
+            res.send({"status": "err"});
         }
     });
 });
